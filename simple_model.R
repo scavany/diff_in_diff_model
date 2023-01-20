ec4da63132aeb814095960f8203f111cf8db919f
Sean Cavany <scavany@nd.edu>
2023-01-20 14:14:26 +0000

    first set of plots

--- simple_model.R
+++ simple_model.R
@@ -2,58 +2,81 @@ if(!require(pacman)){install.packages('pacman');library(pacman)}
 p_load(deSolve,
        adaptivetau)
 
-sir.model <- function(t, state, parameters)
-{
-    sir.odes <- function(t,state,parameters) {
-        with(as.list(c(state, parameters)),{
-            N <- S + I + R
-            dS <- -beta * I * S / N
-            dI <- beta * I * S / N - gamma * I
-            dR <- gamma * I
-            list(c(dS,dI,dR))
-        })
-    }
-    out <- as.data.frame(ode(state,t,sir.odes,parameters))
-    return(out)
-}
+source("./diff_in_diff_fn.R")
 
-sir.stoch <- function(t, state, parameters){
-    ## Transitions matrix
-    sir.transitions <- list(
-        c(I=1,S=-1),
-        c(R=1,I=-1)
-    )
-    ## Rates function
-    sir.rates <- function(state, parameters, t)
-    {
-        with(as.list(c(state, parameters)),{
-            N <- S + I + R
-            return(c(beta * I * S / N,
-                     gamma * I))
-        })
-    }
-    
-    out.temp <- as.data.frame(adaptivetau::ssa.adaptivetau(state,  sir.transitions,
-                                                           sir.rates,
-                                                           parameters, tf=diff(range(t))))
-    out.temp$time <- out.temp$time + min(t)
-    out <- cbind(time = t, apply(out.temp[, -1], 2, function(col) {
-        approx(x = out.temp[, 1], y = col, xout = t, method = "constant")$y
-    }))
-    return(as.data.frame(out))
-}
-
-
-state.init <- c(S=9999,I=1,R=0)
-beta <- 1
-gamma <- 1/5
+state.1 <- c(S=899,I=1,R=100)
+state.2 <- c(S=890,I=10,R=100)
+beta <- 1/15
+gamma <- 1/30
 parms <- c(beta=beta,gamma=gamma)
-out.det <- sir.model(seq(0,60,0.1),state.init,parms)
-out.stoch <- sir.stoch(seq(0,60,0.1),state.init,parms)
+out.det.1 <- sir.model(seq(0,60,0.1),state.1,parms)
+out.det.2 <- sir.model(seq(0,60,0.1),state.2,parms)
 
-plot(out.det$time,out.det$S,col="red",type='l',lwd=3)
-lines(out.det$time,out.det$I,col="green",lwd=3)
-lines(out.det$time,out.det$R,col="blue",lwd=3)
-lines(out.stoch$time,out.stoch$S,col="red",lwd=3,lty=2)
-lines(out.stoch$time,out.stoch$I,col="green",lwd=3,lty=2)
-lines(out.stoch$time,out.stoch$R,col="blue",lwd=3,lty=2)
+## Same parameters, different initial conditions
+## Difference in difference of prevalence at different time points
+## Null intervention at t=0 
+par(mfrow=c(1,2))
+plot(out.det.1$time,out.det.1$I,col="red",type='l',lwd=3,log="y")
+lines(out.det.1$time,out.det.2$I,col="green",lwd=3)
+plot(out.det.1$time,(out.det.1$I-out.det.2$I)-(state.1[["I"]]-state.2[["I"]]),type='l',lwd=3,xlab="time (days)",ylab="DiD")
+abline(h=0)
+abline(h=-(state.1[["I"]]-state.2[["I"]]),lty=2)
+
+## Difference in difference of log(prevalence) at different time points
+## Null intervention at t=0 
+par(mfrow=c(1,2))
+plot(out.det.1$time,out.det.1$I,col="red",type='l',lwd=3,log="y")
+lines(out.det.1$time,out.det.2$I,col="green",lwd=3)
+lines(out.det.1$time,out.det.2$I/(state.2[["I"]]/state.1[["I"]]),col="green",lwd=3,lty=2)
+plot(out.det.1$time,(log(out.det.1$I)-log(out.det.2$I))-(log(state.1[["I"]])-log(state.2[["I"]])),
+     type='l',lwd=3,xlab="time (days)",ylab="DiD")
+
+## Difference in difference of Rt 
+par(mfrow=c(1,2))
+plot(out.det$time,out.det.1$I,col="red",type='l',lwd=3)
+lines(out.det$time,out.det.2$I,col="green",lwd=3)
+R0 <- parms[["beta"]]/parms[["gamma"]]
+Rt.1 <- R0 * out.det.1$S/(out.det.1$S+out.det.1$I+out.det.1$R)
+Rt.2 <- R0 * out.det.2$S/(out.det.2$S+out.det.2$I+out.det.2$R)
+plot(out.det.1$time,(Rt.1-Rt.2)-(Rt.1[1]-Rt.2[1]),
+     type='l',lwd=3,xlab="time (days)",ylab="DiD")
+abline(h=0,lty=2)
+
+par(mfrow=c(1,2))
+plot(out.det.1$time,out.det.1$I,col="red",type='l',lwd=3)
+lines(out.det.1$time,out.det.2$I,col="green",lwd=3)
+plot(out.det.1$R+out.det.1$I,Rt.1,type='l',col='red')
+lines(out.det.2$R+out.det.2$I,Rt.2,col='green')
+
+## time-varying beta
+parms.t <- c(beta=beta,gamma=gamma,beta.0=beta,amp=1/30)
+out.det.1 <- sir.model(seq(0,360,0.1),state.1,parms.t)
+out.det.2 <- sir.model(seq(0,360,0.1),state.2,parms.t)
+par(mfrow=c(1,2))
+plot(out.det.1$time,out.det.1$I,col="red",type='l',lwd=3)
+lines(out.det.2$time,out.det.2$I,col="green",lwd=3)
+plot(out.det.1$time,beta.t(out.det.1$time,parms.t[["beta.0"]],parms.t[["amp"]]),type='l',lwd=2)
+
+## Plot the Rt against cumulative incidence
+## Check the lines are the same
+par(mfrow=c(1,3))
+plot(out.det.1$time,out.det.1$I,col="red",type='l',lwd=3,xlab="time (days)",ylab="I")
+lines(out.det.1$time,out.det.2$I,col="green",lwd=3)
+plot(out.det.1$R+out.det.1$I,Rt.1,type='l',col='red',xlab="Cumulative incidence",ylab="Rt"))
+lines(out.det.2$R+out.det.2$I,Rt.2,col='green')
+RtvCuminc <- approxfun(out.det.1$R+out.det.1$I,Rt.1)
+plot(out.det.1$R+out.det.1$I,RtvCuminc(out.det.2$R+out.det.2$I)-Rt.2,xlab="Cumulative incidence",ylab="Difference in Rt estimates")
+
+## Plot the Rt against cumulative incidence
+## Check the lines are the same
+parms.t1 <- c(beta=beta,gamma=gamma,beta.0=beta,amp=1/30)
+parms.t2 <- c(beta=beta,gamma=gamma,beta.0=beta,amp=1/30)
+out.det.1 <- sir.model(seq(0,360,0.1),state.1,parms.t)
+out.det.2 <- sir.model(seq(0,360,0.1),state.2,parms.t)
+par(mfrow=c(1,3))
+plot(out.det.1$time,out.det.1$I,col="red",type='l',lwd=3,xlab="time (days)",ylab="I")
+lines(out.det.1$time,out.det.2$I,col="green",lwd=3)
+plot(out.det.1$R+out.det.1$I,Rt.1,type='l',col='red',xlab="Cumulative incidence",ylab="Rt"))
+lines(out.det.2$R+out.det.2$I,Rt.2,col='green')
+RtvCuminc <- approxfun(out.det.1$R+out.det.1$I,Rt.1)
+plot(out.det.1$R+out.det.1$I,RtvCuminc(out.det.2$R+out.det.2$I)-Rt.2,xlab="Cumulative incidence",ylab="Difference in Rt estimates")
